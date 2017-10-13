06b06ab0351d882af8ffb12a0257362685d7fa9f
dnnsys <as@rsdigital.ru>
2017-10-13 05:05:35 +0300

    finished 3 sections

--- resources/assets/javascripts/Application.js
+++ resources/assets/javascripts/Application.js
@@ -1,10 +1,18 @@
 //VENDORS
 window.$ = window.jQuery = require('jquery');
 require('bootstrap-sass');
+let AOS = require('aos');
 
 class Application{
     constructor(){
         console.log('application start');
+        this._init();
+    }
+
+    _init(){
+        $(window).on('load', function() {
+            AOS.init();
+        });
     }
 }
 
