3f80ca55c06024dfa7f2c3f9cfd6165c634e710b
dnnsys <as@rsdigital.ru>
2017-10-17 18:10:12 +0300

    added mobile menu, added menu for logined user, changed header

--- resources/assets/javascripts/Application.js
+++ resources/assets/javascripts/Application.js
@@ -9,6 +9,8 @@ class Application{
     constructor(){
         console.log('application start');
         this._init();
+        this._userMenuDropdown();
+        this._burgerMenu();
     }
 
     _init(){
@@ -16,6 +18,27 @@ class Application{
             AOS.init();
         });
         new TestimonialsSlider();
+    }
+
+    _userMenuDropdown(){
+        $('.js-user-menu-dropdown').on('click', function (event) {
+            event.preventDefault();
+            let $this = $(this);
+            $this.toggleClass('open');
+            $this.siblings('.header__user-menu-dropdown').slideToggle();
+        });
+    }
+
+    _burgerMenu(){
+        $('.header__burger').on('click', function () {
+            let $this = $(this);
+            $this.toggleClass('open');
+            $('.header__mobile-menu').slideToggle(function () {
+                if ($(this).is(':visible'))
+                    $(this).css('display','flex');
+            });
+            $('html').toggleClass('opened-menu');
+        });
     }
 }
 
